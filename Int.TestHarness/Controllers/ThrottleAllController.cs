fa9bd04ac7d8587467f4606b38050d4e659336fb
Kez Nwichi <knwichi@hso.com>
2021-08-21 12:05:03 +0100

    main code

---
+++ Int.TestHarness/Controllers/ThrottleAllController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Int.TestHarness.Controllers
+{    
+    [ApiController]
+    [Route("[controller]")]
+    public class ThrottleAllController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(int retrycount)
+        {
+            if (retrycount > 2) return Ok();
+            Response.Headers.Add("Retry-After", "11");
+            return StatusCode(429);
+        }
+
+        [HttpPost]
+        public IActionResult Post(int retrycount)
+        {
+            if (retrycount > 2) return Ok();
+            Response.Headers.Add("Retry-After", "11");
+            return StatusCode(429);
+        }
+
+        [HttpPatch]
+        public IActionResult Patch(int retrycount)
+        {
+            if (retrycount > 2) return Ok();
+            Response.Headers.Add("Retry-After", "11");
+            return StatusCode(429);
+        }
+    }
+}
